d87151031516b2d4eaf7fe5dfa8e0d9b556070da
Phong Kim Do <94376574+Phongdo1104@users.noreply.github.com>
2025-06-01 11:23:53 +0700

    update-my-work-layout - Update zoom

--- src/components/MyWorkBlockVertical.vue
+++ src/components/MyWorkBlockVertical.vue
@@ -194,8 +194,8 @@ function assignScrollTriggerToElement(elementName: string, startScale: number, e
     let tl = gsap.timeline({
         scrollTrigger: {
             trigger: elementName,
-            scrub: 1,
-            start: "top-=100px center",
+            scrub: 2,
+            start: "top center",
             end: "top bottom",
             invalidateOnRefresh: true,
             immediateRender: false,
